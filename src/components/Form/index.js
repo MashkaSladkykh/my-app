eac3a8824e03c9de4a920ef21802080a1ebfd67f
Masha <masha.sladky@gmail.com>
2023-03-23 16:49:05 +0200

    changed dataset

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,14 +2,37 @@ import { useState } from "react";
 
 export const Form =()=>{
     const data = ['Chrome', 'Safari', 'Firefox', 'Shit yandex', 'Chromium', 'Opera'];
-    const [value, setValue] = useState('lol');  
+    const [value, setValue] = useState(''); 
+    const [result, setResult] = useState(data);
+    const [active, setActive] = useState(false);
+
+    const filter = value => data.filter(el => el.toLowerCase().includes(value.toLowerCase()))
+    
+    const handleChange = (e) => {
+        const str = e.target.value;
+        setValue(str);
+        setResult(filter(str))
+    };
+
+    const handleClick =() => {
+        setActive(!active)
+    };
 
     return(
-        <form>
-            <input placeholder="Your browser is..." list="browsers" onChange={(e)=>setValue(e.target.value)}/>
-            <datalist id="browsers">
-                {data.map(browser => <option value={browser} key={browser}></option>)}
-            </datalist>
+        <form className="container">
+            <div className="input-container">
+                <input 
+                placeholder="Your browser is..." 
+                className="input" 
+                onChange={(e)=>handleChange(e)} 
+                onClick={handleClick}
+                value={value}/>
+
+                <ul className={active ? 'list active' : 'list'}>
+                    {result.map(browser => 
+                      <li className="list-item" onClick={()=>{setValue(browser); setResult(filter(browser))}} key={browser}> {browser}</li>)}
+                </ul>
+            </div>
             <button type="submit" onClick={(e)=> {e.preventDefault(); alert(value)}}>Submit</button>
         </form>
     )
