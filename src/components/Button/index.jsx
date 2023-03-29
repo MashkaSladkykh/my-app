d27f35e3cb32bff8721be8859798e1ca6c182fd5
Masha <masha.sladky@gmail.com>
2023-03-29 15:59:14 +0300

    buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,43 +1,46 @@
 import './styles.css';
 
 const getId = ()=> Math.random()+Math.random()*Math.random();
+const calculatePosition = ({top, left, zIndex, side}) => {
+  switch(side) {
+    case 'LT':
+      return { 
+        left: left -50,
+        top:top - 25,
+        zIndex: 1
+    }
+    case 'RT':
+      return { 
+        left: left + 50,
+        top:top - 25,
+        zIndex: 1
+    };
+    case 'LB':
+      return { 
+        left: left -50,
+        top:top + 25,
+        zIndex: 1
+    }
+    case 'RB':
+      return { 
+        left: left + 50,
+        top:top + 25,
+        zIndex: 1
+    }
+    default:
+      return{
+        left, top, zIndex
+    }
+  }
+};
 
 export const Button = ({setButtons, ids, childrens, position}) => {
   const {left, top, zIndex} = position;
-  const calculatePosition = () => {
-    switch(childrens.className) {
-      case 'LT':
-        return { 
-          left: left -50,
-          top:top - 25,
-          zIndex: 1
-      }
-      case 'RT':
-        return { 
-          left: left + 50,
-          top:top - 25,
-          zIndex: 1
-      };
-      case 'LB':
-        return { 
-          left: left -50,
-          top:top + 25,
-          zIndex: 1
-      }
-      case 'RB':
-        return { 
-          left: left + 50,
-          top:top + 25,
-          zIndex: 1
-      }
-      default:
-        return{
-          left, top, zIndex
-      }
-    }
+  
+  const generateChild = () => {
+    
   }
 
 
-
-  return <div>'kek'</div>
+  return <div>Click me</div>
   }
