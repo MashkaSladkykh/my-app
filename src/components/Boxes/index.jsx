8319aee29e500a1a0621bde174df0c541cb211fc
Masha <masha.sladky@gmail.com>
2023-03-30 18:52:20 +0300

    imprived input

--- src/components/Boxes/index.jsx
+++ src/components/Boxes/index.jsx
@@ -3,13 +3,14 @@ import './styles.css';
 export const Box = ({value, setBoxes, id}) => {
     const setValues = e => {
         setBoxes(boxes=>{
-            boxes[id].value = e.target.value;
+            boxes[id].value = e.target.value[e.target.value.length-1];
+            e.target.value = e.target.value[e.target.value.length-1]
             return [...boxes]
         })
     }
 
     return (
-        <input className="box" id={id} maxLength={1} defaultValue={value} onChange={e=> setValues(e)}/>
+        <input className="box" id={id} defaultValue={value} onChange={e=> setValues(e)}/>
     )
 
 }
