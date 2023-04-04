e79a49ecd51ad06cba105b17c6697343342d5431
Masha <masha.sladky@gmail.com>
2023-04-04 17:12:49 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -12,16 +12,11 @@ function App() {
     value: 0,
   });
 
-  let {min, max, values, value} = state;
-
   return (
     <div className="App">
       <Range
        setState={setState}
-       min={min}
-       max={max}
-       values={values}
-       value={value}
+       state={state}
       />
     </div>
   );
--- src/components/Range/index.jsx
+++ src/components/Range/index.jsx
@@ -1,23 +1,38 @@
 
-export const Range = ({setState, min, max, values, value}) => {
-
+export const Range = ({setState, state}) => {
+  let {values} = state;
     const handleSliderClick = (e) => {
       const rect = e.target.getBoundingClientRect();
-      console.log(rect,'rect')
-      const clickX = e.pageX - window.scrollX;
-      console.log(clickX,'clickX')
-      const sliderX = rect.left;
-      const sliderWidth = rect.width;
-      const percentage = ((clickX - sliderX) / sliderWidth) * 100;
-      setState({values: [percentage, values[1]]});
+      const sliderWidth = 500;
+      const percentage = ((e.pageX - rect.left) / sliderWidth) * 100;
+      
+      values[0] < percentage
+       ? 
+       setState({...state, values: [percentage, values[1]]})
+       : setState({...state, values: [values[0], percentage]})
+      
     };
   
     return (
       <div className="wrapper">
-        <div className="ui-slider" onClick={handleSliderClick}>
-          <div className="ui-slider-range" style={{left: `${values[0] > values[1] ? values[1] : values[0] }%`, width: `${values[0] > values[1] ? values[0] - values[1] : values[1] - values[0]}%`}}></div>
-          <span className="ui-slider-handle" tabIndex="0" style={{left: `${values[0]}%`}} onMouseDown={e => e.preventDefault()}></span>
-          <span className="ui-slider-handle" tabIndex="0" style={{left: `${values[1]}%`}} onMouseDown={e => e.preventDefault()}></span>
+        <div className="slider" onClick={handleSliderClick}>
+          <div 
+            className="slider-range" 
+            style={{left: `${values[0] > values[1] ? values[1] : values[0] }%`, 
+            width: `${values[0] > values[1] ? values[0] - values[1] : values[1] - values[0]}%`}}>
+          </div>
+          <span
+            className="slider-handle" 
+            tabIndex="0" 
+            style={{left: `${values[0]}%`}} 
+            onMouseDown={e => e.preventDefault()}>
+          </span>
+          <span 
+            className="slider-handle" 
+            tabIndex="0" 
+            style={{left: `${values[1]}%`}} 
+            onMouseDown={e => e.preventDefault()}>
+          </span>
         </div>
       </div>
     );
