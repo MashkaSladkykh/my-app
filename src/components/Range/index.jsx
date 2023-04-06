c9095791ef7ffa956f761866b1ee830767c8dc2b
Masha <masha.sladky@gmail.com>
2023-04-06 15:42:38 +0300

    now it is working good, added styles for output

--- src/components/Range/index.jsx
+++ src/components/Range/index.jsx
@@ -6,16 +6,20 @@ export const Range = ({setState, state}) => {
     if(e.target.className === "range-min") {
       values[0] = parseInt(e.target.value);
       left = ((values[0] / max) * 100);
+      right = 100 - (values[1] / max) * 100;
     } else {
       values[1] = parseInt(e.target.value);
       right = 100 - (values[1] / max) * 100;
+      left = ((values[0] / max) * 100)
   }} else { 
     if(e.target.className === "range-min") {
       values[0] = parseInt(e.target.value);
       right = 100 - (values[0] / max) * 100;
+      left = ((values[1] / max) * 100);
     } else {
       values[1] = parseInt(e.target.value);
-      left = ((values[1] / max) * 100);
+      left = ((values[1] / max) * 100)
+      right = 100 - (values[0] / max) * 100;
     }
   }
   setState({
@@ -35,11 +39,31 @@ export const Range = ({setState, state}) => {
     if(Math.abs(values[0] - newValue) < Math.abs(values[1] - newValue)) {
       values[0] = newValue;
       left = ((values[0] / max) * 100);
+      right =  100 - (values[1] / max) * 100;
     } else { 
       values[1] = newValue;
       right = 100 - (values[1] / max) * 100;
     }
+    if(values[0] > values[1]) {
+      right = 100 - (values[0] / max) * 100;
+      left = ((values[1] / max) * 100);
+    }
 
+    if(values[0] < min) {
+      values[0]=min;
+      left = min;
+    } else if(values[1] < min) {
+      values[1] = min;
+      left = min;
+    } else if(values[0] > max) {
+      values[0] = max;
+      right = max-max;
+    } else if(values[1] > max) {
+      values[1] = max;
+      right = max-max;
+    }
+
+    console.log(values)
     setState({
       ...state, 
       values,
