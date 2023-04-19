4f71f4b71ea45e386c854309a5caa5f92dc668be
Masha <masha.sladky@gmail.com>
2023-04-19 17:58:03 +0300

    deleted option

--- src/components/Pagination/Select/index.jsx
+++ src/components/Pagination/Select/index.jsx
@@ -4,7 +4,6 @@ export const Select = ({setPostsPerPage}) => {
 
     return(
         <select name="" id="" onChange={(e)=> setPostsPerPage(e.target.value)} className="select">
-            <option value="1">1/page</option>
         <option value="10">10/page</option>
         <option value="20">20/page</option>
         <option value="50">50/page</option>
